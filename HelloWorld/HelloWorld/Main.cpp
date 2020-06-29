49d4b1e9940f1300da842b4b02376644d9efb9b2
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-06-29 13:11:27 +0100

    Loop lesson

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -4,11 +4,17 @@
 
 int main()
 {
-  int x = 6;
-  bool comparisonResult = x == 5;
-  if (comparisonResult)
+  for (int i = 0; i < 5; i++)
   {
     Log("Hello World!");
   }
+
+  int i = 0;
+  while (i < 5)
+  {
+    Log("Hello World!");
+    i++;
+  }
+
   std::cin.get();
 }
