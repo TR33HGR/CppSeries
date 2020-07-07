3afcedb44c624273589a197a8d0bf5bc7ed270c1
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-07-07 13:28:01 +0100

    How to classes

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
 
-#define LOG(x) cout << x << endl
-
-using namespace std;
-
-class Player
+class Log
 {
 public:
+  const int LogLevelError = 0;
+  const int LogLevelWarning = 1;
+  const int LogLevelInfo = 2;
 
-int x, y;
-int speed;
+private:
+  int m_LogLevel = LogLevelInfo;
 
-void Move(int xa, int ya)
-{
-  x += xa * speed;
-  y += ya * speed;
-}
-};
-
-struct Vec2
-{
-  float x, y;
-
-  void Add(const Vec2& other)
+public:
+  void SetLevel(int level)
   {
-    x += other.x;
-    y += other.y;
+    m_LogLevel = level;
+  }
+
+  void Error(const char* message)
+  {
+    if (m_LogLevel >= LogLevelError)
+      std::cout << "[ERROR]: " << message << std::endl;
+  }
+
+  void Warn(const char* message)
+  {
+    if (m_LogLevel >= LogLevelWarning)
+      std::cout << "[WARNING]: " << message << std::endl;
+  }
+
+  void Info(const char* message)
+  {
+    if (m_LogLevel >= LogLevelInfo)
+      std::cout << "[INFO]: " << message << std::endl;
   }
 };
 
 int main()
 {
-  Player player;
-  player.Move(1, -1);
-
+  Log log;
+  log.SetLevel(log.LogLevelWarning);
+  log.Warn("Hello");
   std::cin.get();
 }
