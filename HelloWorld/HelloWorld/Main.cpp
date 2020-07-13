b9f9f5c5ba1ec444089271cae215b169b3237393
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-07-13 10:38:19 +0100

    Enums end lesson

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -3,34 +3,37 @@
 class Log
 {
 public:
-  const int LogLevelError = 0;
-  const int LogLevelWarning = 1;
-  const int LogLevelInfo = 2;
+  enum Level
+  {
+    LevelError = 0,
+    LevelWarning,
+    LevelInfo,
+  };
 
 private:
-  int m_LogLevel = LogLevelInfo;
+  Level m_LogLevel = LevelInfo;
 
 public:
-  void SetLevel(int level)
+  void SetLevel(Level level)
   {
     m_LogLevel = level;
   }
 
   void Error(const char* message)
   {
-    if (m_LogLevel >= LogLevelError)
+    if (m_LogLevel >= LevelError)
       std::cout << "[ERROR]: " << message << std::endl;
   }
 
   void Warn(const char* message)
   {
-    if (m_LogLevel >= LogLevelWarning)
+    if (m_LogLevel >= LevelWarning)
       std::cout << "[WARNING]: " << message << std::endl;
   }
 
   void Info(const char* message)
   {
-    if (m_LogLevel >= LogLevelInfo)
+    if (m_LogLevel >= LevelInfo)
       std::cout << "[INFO]: " << message << std::endl;
   }
 };
@@ -38,7 +41,9 @@ public:
 int main()
 {
   Log log;
-  log.SetLevel(log.LogLevelWarning);
+  log.SetLevel(Log::LevelWarning);
   log.Warn("Hello");
+  log.Error("Hello");
+  log.Info("Hello");
   std::cin.get();
 }
