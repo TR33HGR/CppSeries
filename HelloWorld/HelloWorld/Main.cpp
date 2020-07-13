8eac8052f63a4ac9ee316f8cdb6ccfec38049ef4
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-07-13 10:31:11 +0100

    Revert "Static in classes"
    This reverts commit 757213173941bb96a3315fd5f7e8b0d32a65da44.

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
 
-struct Entity
-{
-  int x, y;
+extern int s_Variable;
 
-  static void Print()
-  {
-    std::cout << x << ", " << y << std::endl;
-  }
-};
-
-static void Print(Entity e)
+void Function()
 {
-  std::cout << e.x << ", " << e.y << std::endl;
+
 }
 
 int main()
 {
-  Entity e;
-  e.x = 2;
-  e.y = 3;
-
-  Entity e1;
-  e1.x = 5;
-  e1.y = 8;
-
-  Entity::Print();
-  Entity::Print();
+  std::cout << s_Variable << std::endl;
   std::cin.get();
 }
