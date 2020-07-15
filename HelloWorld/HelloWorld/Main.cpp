3af53dafd06776286033e55e466a82cb5e2c2ebd
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-07-15 13:13:38 +0100

    Arrays

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <string>
+#include <array>
 
 class Entity
 {
 public:
-  int X, Y;
+  static const int exampleSize = 5;
+  int* example = new int[5];
+  int example2[exampleSize];
 
-  void Print()
-  {}
+  std::array<int, 5> another;
 
-public:
   Entity()
   {
-    X = 0;
-    Print();
-  }
+    int a[5];
+    int count = sizeof(a) / sizeof(int); // 5
+    int count2 = sizeof(example) / sizeof(int); // 0
 
-};
+    for (int i = 0; i < exampleSize; ++i)
+      example[i] = 2;
 
-class Player : public Entity
-{
-public:
-  Player()
-  {
-    X = 2;
-    Print();
+    for (int i = 0; i < another.size(); ++i)
+      another[i] = 2;
   }
 
 };
@@ -32,8 +28,31 @@ public:
 int main()
 {
   Entity e;
-  e.X = 2;
-  e.Print();
+/*
+  int example[5];
+  example[0] = 2;
+  example[4] = 4;
+
+  int* ptr = example;
+
+  for (int i = 0; i < 5; ++i)
+    example[i] = 2;
+
+  example[2] = 5;
+  *(int*)((char*)ptr + 8) = 6;
+  *(ptr + 2) = 7;
+
+  int a = example[0];
+
+  std::cout << example[0] << std::endl;
+  std::cout << example << std::endl;
+*/
+  int* another = new int[5];
+
+  for (int i = 0; i < 5; ++i)
+    another[i] = 2;
+
+  delete[] another;
 
   std::cin.get();
 }
