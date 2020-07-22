d21afe284e1fe2c231773cec0fb8ea727dfd6e6c
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-07-22 16:28:43 +0100

    Mutable

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -4,65 +4,32 @@
 class Entity
 {
 private:
-  int m_X, m_Y;
-  int* m_A, *m_B;
-  mutable int var;
+  std::string m_Name;
+  mutable int m_DebugCount = 0;
 public:
-  int GetX() const
+  const std::string& GetName() const
   {
-    // m_X = 2;
-    var = 2;
-    return m_X;
+    ++m_DebugCount;
+    return m_Name;
   }
-
-  int GetY()
-  {
-    return m_Y;
-  }
-
-  void SetX(int x)
-  {
-    m_X = x;
-  }
-
-  const int* const GetA() const
-  {
-    return m_A;
-  }
-
 };
-
-void PrintEntity(const Entity& e)
-{
-  std::cout << e.GetX() << std::endl;
-  // std::cout << e.GetY() << std::endl;
-}
 
 int main()
 {
-  Entity e;
+  const Entity e;
+  e.GetName();
 
-  const int MAX_AGE = 90;
+  int x = 8;
+  auto f = [=]() mutable
+  {
+    ++x;
+    std::cout << x << std::endl;
+  };
 
-  const int* a = new int;
-  // int const* a = new int;
+  f();
+  f();
 
-  // *a = 2;
-  a = (int*)&MAX_AGE;
-  std::cout << *a << std::endl;
-
-  int* const b = new int;
-
-  *b = 2;
-  // b = (int*)&MAX_AGE;
-  std::cout << *b << std::endl;
-
-  const int* const c = new int;
-
-  //*c = 2;
-  // b = (int*)&MAX_AGE;
-  std::cout << *c << std::endl;
-
+  std::cout << x << std::endl;
 
   std::cin.get();
 }
