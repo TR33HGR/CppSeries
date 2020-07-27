012ae59858eb597d3b6bea16572bd6e290337cc2
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-07-27 16:42:52 +0100

    Create/Instantiate Objects

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 #include <string>
 
-static int s_Level = 1;
-static int s_Speed = 2;
+using String = std::string;
+
+class Entity
+{
+private:
+  String m_Name;
+public:
+  Entity()
+    : m_Name("Unknown")
+  {}
+
+  Entity(const String& name)
+    : m_Name(name)
+  {}
+
+  const String& GetName() const
+  {
+    return m_Name;
+  }
+};
 
 int main()
 {
-  if (s_Level > 5)
-    s_Speed = 10;
-  else
-    s_Speed = 5;
+  Entity* e;
+  {
+    Entity entity("Cherno");
+    e = &entity;
+    std::cout << entity.GetName() << std::endl;
+  }
 
-  s_Speed = s_Level > 5 && s_level < 100 ? s_Level > 10 ? 15 : 10 : 5;
-
-  std::cout << s_Speed << std::endl;
-
-  std::string rank = s_Level > 10 ? "Master" : "Beginner";
-
-  std::string otherRank;
-  if (s_Level > 10)
-    otherRank = "Master";
-  else
-    otherRank = "Beginner";
+  Entity* e2;
+  {
+    Entity* entity = new Entity("Cherno2");
+    e2 = entity;
+    std::cout << entity->GetName() << std::endl;
+  }
 
   std::cin.get();
+  delete e2;
 }
