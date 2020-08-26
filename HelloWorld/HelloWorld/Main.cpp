cfa17410bc517c44eeaf09bc81f77217764bd05c
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-08-26 13:19:30 +0100

    this

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,66 +1,43 @@
 #include <iostream>
 #include <string>
 
-struct Vector2
+void PrintEntity(const Entity&);
+
+class Entity
 {
-  float x, y;
+public:
+  int x, y;
 
-  Vector2(float x, float y)
-    : x{x}
-    , y{y}
-  {}
-
-  Vector2 Add(const Vector2& other) const
+  Entity(int x, int y)
   {
-    //return Vector2{x + other.x, y + other.y};
-    //return *this + other;
-    return operator+(other);
+    Entity* e = this;
+
+    e->x = x;
+    this->y = y;
+
+    Entity& e = *this;
+
+    PrintEntity(*this);
+
+    delete this;
   }
 
-  Vector2 Multiply(const Vector2& other) const
+  int GetX() const
   {
-    //return Vector2{x * other.x, y * other.y};
-    return *this * other;
-  }
+    const Entity* e = this;
+    const Entity& e2 = *this;
 
-  Vector2 operator+(const Vector2& other) const
-  {
-    return Vector2{x + other.x, y + other.y};
-  }
-
-  Vector2 operator*(const Vector2& other) const
-  {
-    return Vector2{x * other.x, y * other.y};
-  }
-
-  bool operator==(const Vector2& other) const
-  {
-    return x == other.x && y == other.y;
-  }
-
-  bool operator!=(const Vector2& other) const
-  {
-    return !(*this == other);
-    //return !operator==(other);
+    return x;
   }
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vector2& other)
+void PrintEntity(const Entity& e)
 {
-  stream << other.x << ", " << other.y;
-  return stream;
+    // Print
 }
 
 int main()
 {
-  Vector2 position{4.0f, 4.0f};
-  Vector2 speed{0.5f, 0.5f};
-  Vector2 powerup{1.1f, 1.1f};
-
-  Vector2 result = position.Add(speed.Multiply(powerup));
-  Vector2 result2 = position + speed * powerup;
-
-  std::cout << result2 << std::endl;
 
   std::cin.get();
 }
