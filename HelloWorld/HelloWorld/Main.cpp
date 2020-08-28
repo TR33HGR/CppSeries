a2f58fdea9d33574e2273be0f0be03781a9501a2
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-08-28 14:38:51 +0100

    Copying

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
 #include <string>
-#include <memory>
 
-class Entity
+class String
 {
+private:
+  char* m_Buffer;
+  unsigned int m_Size;
 public:
-  Entity()
+  String(const char* string)
   {
-    std::cout << "Created Entity!" << std::endl;
+    m_Size = strlen(string);
+    m_Buffer = new char[m_Size + 1];
+    memcpy(m_Buffer, string, m_Size);
+    m_Buffer[m_Size] = 0;
   }
 
-  ~Entity()
+  String(const String& other)
+    : m_Size{other.m_Size}
   {
-    std::cout << "Destroyed Entity!" << std::endl;
+    std::cout << "Copy string" << std::endl;
+    m_Buffer = new char[m_Size + 1];
+    memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
   }
 
-  void Print()
-  {}
+  char& operator[](unsigned int index)
+  {
+    return m_Buffer[index];
+  }
+
+  ~String()
+  {
+    delete[] m_Buffer;
+  }
+
+  friend std::ostream& operator<<(std::ostream& stream, const String& string);
 };
 
+std::ostream& operator<<(std::ostream& stream, const String& string)
+{
+  stream << string.m_Buffer;
+  return stream;
+}
+
+//void PrintString(const String& string)
+void PrintString(String string)
+{
+  std::cout << string << std::endl;
+}
 
 int main()
 {
-  {
-    std::shared_ptr<Entity> e0;
-    std::weak_ptr<Entity> weakEntity;
-    {
-      std::unique_ptr<Entity> entity{new Entity};
-      std::unique_ptr<Entity> entity2 = std::make_unique<Entity>();
-      //std::unique_ptr<Entity> e0 = entity;
-      std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
-      weakEntity = sharedEntity;
-      e0 = sharedEntity;
+  String string = "Cherno";
+  String second = string;
 
-      entity->Print();
-      entity2->Print();
-    }
-  }
+  second[2] = 'a';
+
+  PrintString(string);
+  PrintString(second);
 
   std::cin.get();
 }
