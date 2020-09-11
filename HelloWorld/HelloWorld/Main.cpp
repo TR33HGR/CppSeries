f74b6f73c4371a4006492f39cf199c2892d49635
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-09-11 12:13:47 +0100

    Using libs

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,45 +1,10 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
-struct Vertex
-{
-  float x, y, z;
-
-  Vertex(float x, float y, float z)
-    : x{ x }
-    , y{ y }
-    , z{ z }
-  {
-  }
-
-  Vertex(const Vertex& vertex)
-    : x{ vertex.x }
-    , y{ vertex.y }
-    , z{ vertex.z }
-  {
-    std::cout << "Copied!" << std::endl;
-  }
-};
-
-std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
-{
-  stream << vertex.x << ", " << vertex.y << ", " << vertex.z;
-  return stream;
-}
-
-void Function(const std::vector<Vertex>& vertices)
-{
-
-}
+#include <GLFW/glfw3.h>
 
 int main()
 {
-  std::vector<Vertex> vertices;
-  vertices.reserve(3);
-  vertices.emplace_back(1, 2, 3);
-  vertices.emplace_back(4, 5, 6);
-  vertices.emplace_back(7, 8, 9);
+  int a = glfwInit();
+  std::cout << a << std::endl;
 
   std::cin.get();
 }
