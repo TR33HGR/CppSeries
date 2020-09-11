17e6132bca33efc3205ae49d826a6990a880ecec
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-09-11 08:14:44 +0100

    Optimising vectors

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -5,6 +5,21 @@
 struct Vertex
 {
   float x, y, z;
+
+  Vertex(float x, float y, float z)
+    : x{ x }
+    , y{ y }
+    , z{ z }
+  {
+  }
+
+  Vertex(const Vertex& vertex)
+    : x{ vertex.x }
+    , y{ vertex.y }
+    , z{ vertex.z }
+  {
+    std::cout << "Copied!" << std::endl;
+  }
 };
 
 std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
@@ -21,17 +36,10 @@ void Function(const std::vector<Vertex>& vertices)
 int main()
 {
   std::vector<Vertex> vertices;
-  vertices.push_back({1, 2, 3});
-  vertices.push_back({4, 5, 6});
-
-  for (int i = 0; i < vertices.size(); i++)
-    std::cout << vertices[i] << std::endl;
-
-  vertices.erase(vertices.begin() + 1);
-  vertices.clear();
-
-  for (Vertex& v : vertices)
-    std::cout << v << std::endl;
+  vertices.reserve(3);
+  vertices.emplace_back(1, 2, 3);
+  vertices.emplace_back(4, 5, 6);
+  vertices.emplace_back(7, 8, 9);
 
   std::cin.get();
 }
