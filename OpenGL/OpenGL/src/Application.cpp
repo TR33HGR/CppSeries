f97fe7b433fb6113c981ef5f32c23ca2e4b5eced
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-10-05 08:19:28 +0100

    OpenGL: Vertex Attributes

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -35,9 +35,12 @@ int main(void)
 
   unsigned int buffer;
   glGenBuffers(1, &buffer);
-  // select this buffer, all subsequent commands are on this buffer, until new bind call
+  // bind this buffer, all subsequent commands are on this buffer, until new bind call
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
   glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
