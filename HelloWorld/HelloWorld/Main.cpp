83d4bb36ae224304ae9a844fd3255517391aac8e
Nathaniel Micklewright <nathaniel.micklewright@displaylink.com>
2020-10-19 08:23:01 +0100

    Templates

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-#include <GLFW/glfw3.h>
+#include <string>
+
+template<typename T>
+void Print(T value)
+{
+  std::cout << value <<std::endl;
+}
+
+template<typename T, int N>
+class Array
+{
+private:
+  T m_Array[N];
+public:
+  int GetSize() const { return N; }
+};
 
 int main()
 {
-  int a = glfwInit();
-  std::cout << a << std::endl;
+  Print(5);
+  Print(5.5f);
+
+  Array<std::string, 50> array;
+  Print(array.GetSize());
 
   std::cin.get();
 }
