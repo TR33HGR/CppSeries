2ef00677be292bf1a66ccb669f48da2fc7a1dfcc
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-11-20 13:22:35 +0000

    auto types

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
 
-#if PR_DEBUG == 1
-#define LOG(x) std::cout << x << std::endl
-#elif defined(PR_RELEASE)
-#define LOG(x)
-#endif
-
-#define MAIN int main()\
-{\
-  LOG("Hello");\
-  std::cin.get();\
+std::string GetName()
+{
+  return "Cherno";
 }
 
-MAIN
+class Device {};
+
+class DeviceManager
+{
+private:
+  std::unordered_map<std::string, std::vector<Device*>> m_Devices;
+
+public:
+  const std::unordered_map<std::string, std::vector<Device*>>& GetDevices()
+  {
+    return m_Devices;
+  }
+};
+
+int main()
+{
+  // Bad case
+  auto name = GetName();
+  int a = name.size();
+
+  std::vector<std::string> strings;
+  strings.push_back("Apple");
+  strings.push_back("Orange");
+
+  for (auto it = strings.begin();
+    it != strings.end(); it++) {
+    std::cout << *it << std::endl;
+  }
+
+  //using DeviceMap = std::unordered_map<std::string, std::vector<Device*>>;
+  //typedef std::unordered_map<std::string, std::vector<Device*>> DeviceMap;
+
+  DeviceManager dm;
+  const auto& devices = dm.GetDevices();
+
+  std::cin.get();
+}
