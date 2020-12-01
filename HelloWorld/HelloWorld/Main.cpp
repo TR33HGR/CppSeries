ac7ef85c4f44583537532a4eacbeec35757d3971
Nathaniel Micklewright <nmicklew@synaptics.com>
2020-12-01 08:42:40 +0000

    Lambdas

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <algorithm>
 
 void HelloWorld(int a)
 {
   std::cout << "Hello World! Value: " << a << std::endl;
 }
 
-void ForEach(const std::vector<int>& values, void(*func)(int))
+void ForEach(const std::vector<int>& values, const std::function<void(int)>& func)
 {
   for (int value : values)
     func(value);
@@ -24,7 +26,14 @@ int main()
   function(10);
 
   std::vector<int> values = { 1, 5, 4, 2, 3 };
-  ForEach(values, [](int value){ std::cout << "Value: " << value << std::endl; });
+  auto it = std::find_if(values.begin(), values.end(), [](int value){ return value > 3; });
+  std::cout << *it << std::endl;
+
+  int a = 5;
+
+  auto lambda = [=](int value)mutable{ a = 6; std::cout << "Value: " << value << " " << a << std::endl; };
+
+  ForEach(values, lambda);
 
   std::cin.get();
 }
