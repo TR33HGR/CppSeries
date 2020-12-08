3705bf42ab128770182cfd9e8ada1869b7bd125f
Nathaniel Micklewright <nmicklew@synaptics.com>
2020-12-08 08:31:51 +0000

    Why specify namespace & not using namespace

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <functional>
+#include <string>
 #include <algorithm>
 
-void HelloWorld(int a)
-{
-  std::cout << "Hello World! Value: " << a << std::endl;
+namespace apple {
+
+  void print(const std::string& text)
+  {
+    std::cout << text << std::endl;
+  }
 }
 
-void ForEach(const std::vector<int>& values, const std::function<void(int)>& func)
-{
-  for (int value : values)
-    func(value);
+namespace orange {
+
+  void print(const char* text)
+  {
+    std::string temp = text;
+    std::reverse(temp.begin(), temp.end());
+    std::cout << temp << std::endl;
+  }
 }
 
 int main()
 {
-  typedef void(*HelloWorldFunction)(int);
-
-  HelloWorldFunction function = HelloWorld;
-
-  function(8);
-  function(3);
-  function(2);
-  function(10);
-
-  std::vector<int> values = { 1, 5, 4, 2, 3 };
-  auto it = std::find_if(values.begin(), values.end(), [](int value){ return value > 3; });
-  std::cout << *it << std::endl;
-
-  int a = 5;
-
-  auto lambda = [=](int value)mutable{ a = 6; std::cout << "Value: " << value << " " << a << std::endl; };
-
-  ForEach(values, lambda);
+  apple::print("Hello");
 
   std::cin.get();
 }
